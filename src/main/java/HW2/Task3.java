1665efc84535f60c46b6e807cb24ef582a54d930
KAMaz64 <konstantin.a.mazunin@gmail.com>
2023-08-01 19:48:49 +0300

    Task3 done

---
+++ src/main/java/HW2/Task3.java
@@ -0,0 +1,31 @@
+package HW2;
+
+import java.io.FileNotFoundException;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[2] = 9; // Исправляем индекс, чтобы не возникало ArrayIndexOutOfBoundsException
+        } catch (ArithmeticException ex) {
+            System.out.println("Деление на ноль запрещено!");
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (FileNotFoundException ex) {
+            System.out.println("Ошибка: файл не найден!");
+        } catch (Exception ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+}
