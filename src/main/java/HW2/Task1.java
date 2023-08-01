df466af48086f7d4e070f7af3176b55666e75e62
KAMaz64 <konstantin.a.mazunin@gmail.com>
2023-08-01 19:20:34 +0300

    Task1 done

---
+++ src/main/java/HW2/Task1.java
@@ -0,0 +1,37 @@
+/*
+Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+и возвращает введенное значение.
+Ввод текста вместо числа не должно приводить к падению приложения,
+вместо этого, необходимо повторно запросить у пользователя ввод данных
+*/
+
+package HW2;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        float userInput = readFloatFromUser();
+        System.out.println("Вы ввели: " + userInput);
+    }
+
+    public static float readFloatFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        float userInput = 0.0f;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print("Введите дробное число: ");
+
+            try {
+                userInput = Float.parseFloat(scanner.nextLine());
+                validInput = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка: Введено некорректное значение. Пожалуйста, повторите ввод.");
+            }
+        }
+
+        return userInput;
+    }
+}
